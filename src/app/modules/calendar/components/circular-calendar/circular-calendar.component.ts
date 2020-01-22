af4035559744da8ab9ccc49b91fcfab819594693
Tchaka Spierings <tchaka.spierings@gmail.com>
2020-01-22 16:22:10 +0100

    Keep selected month and rotate in the shortest direction

--- src/app/modules/calendar/components/circular-calendar/circular-calendar.component.ts
+++ src/app/modules/calendar/components/circular-calendar/circular-calendar.component.ts
@@ -1,34 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
-import { isUndefined } from 'util';
 
 @Component({
   selector: 'app-circular-calendar',
   templateUrl: './circular-calendar.component.html',
   styleUrls: ['./circular-calendar.component.scss']
 })
-export class CircularCalendarComponent implements OnInit {
+export class CircularCalendarComponent {
 
   public months: Array<string> = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  public selectedMonth = 0;
+  public isFocused = false;
 
-  private selectedMonth: number;
+  private rotationOffset = 0;
 
   constructor(private sanitizer: DomSanitizer) { }
 
-  ngOnInit() {
-  }
-
   public getTransform(i: number): SafeStyle {
-    const offset = isUndefined(this.selectedMonth) ? 0 : this.selectedMonth;
-    const rotation = 360 / 12 * (i - offset);
-    const translation = 1300; // isUndefined(this.selectedMonth) ? 300 : 1200;
-    const scale = isUndefined(this.selectedMonth) ? 0.25 : 1.0;
+    const rotation = 30 * i + this.rotationOffset;
+    const scale = this.isFocused ? 1.0 : 0.25;
 
-    return this.sanitizer.bypassSecurityTrustStyle(`scale(${scale}) rotate(${rotation}deg) translateY(-${translation}px)`);
+    return this.sanitizer.bypassSecurityTrustStyle(`scale(${scale}) rotate(${rotation}deg) translateY(-1300px)`);
   }
 
   public selectMonth(monthIndex: number) {
-    this.selectedMonth = monthIndex === this.selectedMonth ? undefined : monthIndex;
+    const res = this.closestRotation(this.selectedMonth, monthIndex);
+    this.rotationOffset += res;
+
+    if (this.selectedMonth === monthIndex) {
+      this.isFocused = !this.isFocused;
+    } else {
+      this.isFocused = true;
+    }
+
+    this.selectedMonth = monthIndex;
+  }
+
+  /**
+   * Get the closest rotational distance towards the targetIndex, based on a 12-distance space.
+   */
+  private closestRotation(currentIndex: number, targetIndex: number): number {
+    const absoluteClockwiseDistance = Math.abs(currentIndex - targetIndex);
+    const smallestDistance = Math.min(absoluteClockwiseDistance, 12 - absoluteClockwiseDistance);
+    const direction = currentIndex - targetIndex < 0 ? -1 : 1;
+    const reverse = absoluteClockwiseDistance > 6 ? -1 : 1;
+    return smallestDistance * (direction * reverse) * 30;
   }
 
   public getDaysOfMonth(monthIndex: number) {
